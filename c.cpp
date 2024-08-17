c98118971020712669a36a070f881ca6eaac9d71
Yermek Tairkhan <trouzerop@gmail.com>
2024-08-17 17:49:52 +0600

    new c++

--- c.cpp
+++ c.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int a = 4;
-    int b = 3;
-    cout <<"HI" << "\n";
-    cout << b + a;
-    //afksjfaksfsjfjas
+
+int main()
+{
+   int a = 0;
+   std::cout << "Hello world!"<< endl;
+   int j= 4;
+   while(true){
+      cout<<"hello"<<endl;
+      
+      cin >> a ;
+      if(a==j){
+         break;
+      }
+
+   }
+   return 0;
+   
 }
